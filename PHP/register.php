824706e5fef2e255b94e26d6a0447356a0f977ce
MariaMiguel0510 <mariammpedro04@gmail.com>
2024-11-10 21:07:10 +0000

    Estilização
    Estilizei a página do register e acrescentei um ou dois contentores no html

--- PHP/register.php
+++ PHP/register.php
@@ -4,23 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <main>
-        <h2>Sign Up!</h2>
-        <form>
-            <div>
-                <label for="nome">Name</label>
-                <input type="text" id="nome" name="nome" placeholder="Name" required>
-            </div>
-            <div>
-                <label for="mail">Email</label> 
-                <input type="email" id="mail" name="mail" placeholder="fastfurious@gmail.com" required>
-            </div>
-            <div>
-                <label for="pwd">Password</label>
-                <input type="password" id="pwd" name="pwd" required>
-            </div>
+      <div class="wrapper">
+         <div class="imgSignUp">
+             <img id="imgSU" src="carro.jpg" alt="imagem_signUp" >
+         </div>
+        <div class="outContainer">
+            <div class="container">  
+                <h2 id="titulo">Sign Up</h2>
+                <div class="register_form">
+                    <form>
+                        <div>
+                            <label for="nome">Name</label>
+                            <input type="text" id="nome" name="nome" placeholder="Name" required>
+                        </div>
+                        <div>
+                            <label for="mail">Email</label>
+                            <input type="email" id="mail" name="mail" placeholder="fastfurious@gmail.com" required>
+                        </div>
+                        <div>
+                            <label for="pwd">Password</label>
+                            <input type="password" id="pwd" name="pwd" required>
+                        </div>
 <!-- 
             <div>
                 <label for="nome">Birth Date</label>
@@ -31,10 +39,14 @@
                 <input type="tel" id="phone" name="phone" placeholder="+(239)9123917916" required>
             </div>
 -->
-            <div>
-                <input type="submit" value="Submeter">
+                        <div>
+                            <input id="botao" type="submit" value="Submit">
+                        </div>
+                    </form>
+                </div>
             </div>
-        </form>
+        </div>
+      </div>
     </main>
 </body>
 </html>
