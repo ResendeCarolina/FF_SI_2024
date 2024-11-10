f533a95d70aa22621338ad12cd60784c8248185b
MariaMiguel0510 <mariammpedro04@gmail.com>
2024-11-10 12:03:37 +0000

    Register
    Iniciei o html do formulário do register

---
+++ PHP/register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <main>
+        <h2>Sign Up!</h2>
+        <form>
+            <div>
+                <label for="nome">Name</label>
+                <input type="text" id="nome" name="nome" placeholder="Name" required>
+            </div>
+            <div>
+                <label for="mail">Email</label> 
+                <input type="email" id="mail" name="mail" placeholder="fastfurious@gmail.com" required>
+            </div>
+            <div>
+                <label for="pwd">Password</label>
+                <input type="password" id="pwd" name="pwd" required>
+            </div>
+<!-- 
+            <div>
+                <label for="nome">Birth Date</label>
+                <input type="date" id="data" name="data" required>
+            </div>
+            <div>
+                <label for="phone">Contacto telefónico:</label> 
+                <input type="tel" id="phone" name="phone" placeholder="+(239)9123917916" required>
+            </div>
+-->
+        </form>
+    </main>
+</body>
+</html>
