7832da6d77c3d4d823e9703b05617c895bca7951
MariaMiguel0510 <mariammpedro04@gmail.com>
2024-11-12 23:41:34 +0000

    criei a homepage
    Introduzi o codigo de css e html da homepage

--- PHP/cliente/homepage.php
+++ PHP/cliente/homepage.php
@@ -3,13 +3,102 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Homepage</title>
+    <link rel="stylesheet" href="main.css">
+    
 </head>
 <body>
-    Homepage do Cliente
+    <header>
+        <div class="logotipo">
+            <a href="https://1a85f23f-77e0-4c25-8384-d87848e462e6-00-2wdwtsjhuyghq.worf.replit.dev/">
+              <img id="logo" href="#firstSection" src="IMAGENS/LogoBranco.png" alt="logotipo">
+            </a>
+        </div>
+        
+        <div class="menuContainer">
+            <nav class="menu">
+                <a class="seccao" id="secao1" href="#secondSection">ABOUT US</a>
+                <a class="seccao" id="secao2" href="#thirddSection">PRODUCTS</a>
+                <a class="seccao" id="secao3" href="#fourthSection">CONTACTS</a>
+            </nav>
+        </div>
 
-   
-
+        <div class="icones">
+            <a>
+              <img id="perfil" src="../IMAGENS/pictogramaPerfil.png" alt="perfil">
+            </a>
+        </div>
+    </header>
+    <main>
+        <article>
+            <section class="firstSection">
+                <div class="videoInicial">
+                    <img class="video" src="../IMAGENS/carro.jpg" alt="videoInicial">
+                </div>
+                <div class="sloganInicial">
+                    <p id="slogan">
+                         GO FAST<br>
+                         AND FURIOUS.
+                    </p>
+                </div>
+            </section>
+            <section class="secondSection" id="secondSection">
+                <div class="textContainer">
+                    <div class="text">
+                        <h2 class="titulo">ABOUT US</h2>
+                        <p>Pop art has had an indelible influence on the visual landscape of art and design in the 20th and 21st
+                      centuries. This movement, which emerged in the 1950s and 1960s, was heavily inspired by graphic design
+                      elements and techniques, and is still a major influence in the world of art and design today. From its
+                      roots in advertising and the commercialization of everyday objects to its bold, vibrant, and often
+                      humorous visuals, pop art has been an integral part of graphic design for decades.</p>
+                        <p>Andy Warhol, Roy Lichtenstein, and James Rosenquist are just a few of the many American pop artists who
+                      worked in the commercial art industry prior to becoming famous. As a result, they were capable of
+                      combining both high art and popular culture. Pop art in modern graphic design is represented by a work
+                      created by a designer who was heavily influenced by the movement.</p>
+                        <br>The term “pop art” refers to an art movement that advocated and chastised the commercial arts. Pop art
+                    has been incorporated into the design and advertising of graphic art and advertising in recent years.</br>
+                        <p>Pop art is heavily influenced by graphic design because it relies heavily on the use of recognizable
+                      shapes, colors, and typography. Pop art also utilizes a variety of visual elements, including photography,
+                      text, and illustration, to create an artistic epresentation of popular culture. Graphic design is used to
+                      bridge the gap between the physical and the digital, enabling artists to create visual messages that are
+                      more engaging and memorable. Graphic design allows the artist to craft a message that is more easily
+                      identifiable and can be used to communicate a message in a more interesting manner. By utilizing the
+                      principles of graphic design, pop art can express a message more clearly, making it more impactful and
+                      memorable.</p>
+                    </div>
+                </div>
+            </section>
+            <section class="thirdSection" id="thirdSection">
+                <div class="checkthisout">
+                    <div class="containerPainel">
+                        <h2 class="titulo">CHECK THIS OUT</h2>
+                            <img class="imgPainel" src="../IMAGENS/ctocarro1.jpg" alt="iconeMail">
+                          
+                            <img class="imgPainel" src="../IMAGENS/ctocarro3.jpg" alt="iconeMail">
+                    </div>
+                </div>
+            </section>
+        </article>
+    </main>
+    <footer class="fourthSection" id="fourthSection">
+        <div class="conteudoFooter" id="aluno">
+            <div class="iconesContact">
+                <img class="icoC" id="mail" src="../IMAGENS/pictogramaMail.jpg" alt="iconeMail">
+                <img class="icoC" id="phone" src="../IMAGENS/pictogramaTelefone.jpg" alt="iconePhone">
+                <img class="icoC" id="local" src="../IMAGENS/pictogramaLocalizacao.jpg" alt="iconeLocal">
+            </div>
+            <ul>
+                <li>fastandfurious@gmail.com</li>
+                <li>+351 239 567 307</li>
+                <li>Rua Quinta da Beira, lote 3, nº 25, 3030-240, Coimbra</li>
+            </ul>
+            <div class="iconesMedia">
+                <img class="icoM" id="face" src="../IMAGENS/pictogramaFace.png" alt="iconeFacebook">
+                <img class="icoM" id="insta" src="../IMAGENS/pictogramaInsta.png" alt="iconeInsta">
+                <img class="icoM" id="twitter" src="../IMAGENS/pictogramaTwitter.png" alt="iconeTwitter">
+            </div>
+        </div>        
+    </footer>
     <script src="../JS/script.js"></script>
 </body>
 </html>
