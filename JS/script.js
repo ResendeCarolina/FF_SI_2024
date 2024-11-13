8486da03ed84aaf511f7513c214a6c39132b0085
MariaMiguel0510 <mariammpedro04@gmail.com>
2024-11-13 00:24:12 +0000

    Update script.js
    Adicionei o código de animação

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+let video = document.querySelector('.firstSection');
+
+window.addEventListener('scroll', function() {
+  // Calculando a opacidade com base no scroll, com limite máximo de 1 e mínimo de 0.
+  let value = 1 - window.scrollY / 800; 
+  // Garantir que a opacidade não seja maior que 1 nem menor que 0
+  value = Math.max(0, Math.min(1, value)); 
+  video.style.opacity = value;
+});
