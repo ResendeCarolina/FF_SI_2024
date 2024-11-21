fb4faa4dd7619ec99a20ec17f66911d32ad3141f
MariaMiguel0510 <mariammpedro04@gmail.com>
2024-11-21 16:39:09 +0000

    Header
    Criei a animação que faz o header aparecer suavemente depois de se fazer scroll

--- JS/script.js
+++ JS/script.js
@@ -8,7 +8,7 @@ let handleSubmit = () => {
   console.log(registationForm)
 }
 
-
+//reduz suavemente a opacidade do videoInicial através do scroll
 let video = document.querySelector('.firstSection');
 
 window.addEventListener('scroll', function() {
@@ -17,4 +17,14 @@ window.addEventListener('scroll', function() {
   // Garantir que a opacidade não seja maior que 1 nem menor que 0
   value = Math.max(0, Math.min(1, value)); 
   video.style.opacity = value;
+});
+
+//aumenta suavemente a opacidade do header através do scroll
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+  // Calcula a opacidade com base no scroll, variando entre 1 (no topo) e 0 (após 600px de scroll)
+  let cabecalhoValor = window.scrollY / 600;
+  cabecalhoValor = Math.max(0, Math.min(1, cabecalhoValor));
+  header.style.background = 'rgba(0, 0, 0, ' + cabecalhoValor + ')';
 });
