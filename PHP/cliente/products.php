ba489dcea5c49052dc192430d5a24032375239b5
MariaMiguel0510 <mariammpedro04@gmail.com>
2024-11-23 18:09:53 +0000

    footer
    Coloquei o footer e corrigir uns erros do header na página products

--- PHP/cliente/products.php
+++ PHP/cliente/products.php
@@ -8,23 +8,23 @@
 </head>
 <body>
 <header class="cabecalhoGeral">
-        <div class="logotipo">
+<div class="logotipo">
             <a href="homepage.php">
-              <img id="logo" src="IMAGENS/LogoBranco.png" alt="logotipo">
+                <img id="logo" src="/IMAGENS/LogoBranco.png" alt="logotipo">
             </a>
         </div>
 
         <div class="menuContainer">
             <nav class="menu">
-                <a class="seccao" id="secao1" href="#secondSection">ABOUT US</a>
-                <a class="seccao" id="secao2" href="products.html">PRODUCTS</a>
-                <a class="seccao" id="secao3" href="#fourthSection">CONTACTS</a>
+                <a class="tituloGeral sobreEfeito" id="secao1" href="#secondSection">ABOUT US</a>
+                <a class="tituloGeral sobreEfeito" id="secao2" href="products.php">PRODUCTS</a>
+                <a class="tituloGeral sobreEfeito" id="secao3" href="#fourthSection">CONTACTS</a>
             </nav>
         </div>
 
         <div class="icones">
             <a>
-              <img id="perfil" src="IMAGENS/pictogramaPerfil.png" alt="perfil">
+                <img id="perfil" src="/IMAGENS/pictogramaPerfil.png" alt="perfil">
             </a>
         </div>
     </header>
@@ -33,6 +33,25 @@
             
         </article>
     </main>
+    <footer class="textoGeral fourthSection" id="fourthSection">
+        <div class="conteudoFooter" id="aluno">
+            <div class="iconesContact">
+                <img class="icoC" id="mail" src="/IMAGENS/pictogramaMail.jpg" alt="iconeMail">
+                <img class="icoC" id="phone" src="/IMAGENS/pictogramaTelefone.jpg" alt="iconePhone">
+                <img class="icoC" id="local" src="/IMAGENS/pictogramaLocalizacao.jpg" alt="iconeLocal">
+            </div>
+            <ul>
+                <li>fastandfurious@gmail.com</li>
+                <li>+351 239 567 307</li>
+                <li>Rua Quinta da Beira, lote 3, nº 25, 3030-240, Coimbra</li>
+            </ul>
+            <div class="iconesMedia">
+                <img class="icoM" id="face" src="/IMAGENS/pictogramaFace.png" alt="iconeFacebook">
+                <img class="icoM" id="insta" src="/IMAGENS/pictogramaInsta.png" alt="iconeInsta">
+                <img class="icoM" id="twitter" src="/IMAGENS/pictogramaTwitter.png" alt="iconeTwitter">
+            </div>
+        </div>
+    </footer>
     <script src="/JS/script.js"></script>
 </body>
 </html>
