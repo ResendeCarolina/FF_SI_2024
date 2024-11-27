13f6e9e1ef9ae86d6b0f7455f8f7e74bae55bfdb
MariaMiguel0510 <mariammpedro04@gmail.com>
2024-11-27 14:34:23 +0000

    Homepage - administrador
    Corrigi a homepage dos administradores para que fique com layout igual à dos clientes nas partes iguais

--- PHP/admin/homepage.php
+++ PHP/admin/homepage.php
@@ -9,25 +9,27 @@
 </head>
 
 <body>
-    <header>
+    <header class="cabecalhoHP">
         <div class="logotipo">
-            <a href="#firstSection">
+            <a href="homepage.php">
                 <img id="logo" src="/IMAGENS/LogoBranco.png" alt="logotipo">
             </a>
         </div>
 
         <div class="menuContainer">
             <nav class="menu">
-                <a class="tituloGeral sobreEfeito" id="secao1" href="#secondSection">ABOUT US</a>
-                <a class="tituloGeral sobreEfeito" id="secao2" href="#thirddSection">PRODUCTS</a>
+                <a class="tituloGeral sobreEfeito" id="secao1" href="homepage.php#secondSection">ABOUT US</a>
+                <a class="tituloGeral sobreEfeito" id="secao2" href="products.php">PRODUCTS</a>
                 <a class="tituloGeral sobreEfeito" id="secao3" href="#fourthSection">CONTACTS</a>
             </nav>
         </div>
 
-        <div class="icones">
-            <a>
-                <img id="perfil" src="/IMAGENS/pictogramaPerfil.png" alt="perfil">
-            </a>
+        <div class="iconesContainer">
+            <img class="icones" id="perfil" src="/IMAGENS/pictogramaPerfil.png" alt="perfil">
+            <div class="cartContainer">
+                <img class="icones" id="cart" src="/IMAGENS/pictogramaCart.png" alt="cart">
+                <span class="countP">0</span>
+            </div>
         </div>
     </header>
     <main>
@@ -46,32 +48,32 @@
             <section class="secondSection" id="secondSection">
                 <div class="textContainer">
                     <div class="textoGeral text">
-                        <h2 class="tituloGeral title">ABOUT US</h2>
-                        <p>
-                            A <strong>Fast & Furious Cars Inc.</strong> é uma empresa de referência no mercado de aluguel de carros de luxo, oferecendo uma experiência única para clientes que buscam elegância, conforto e desempenho.
-                            Com uma frota composta pelos veículos mais sofisticados do mercado, desde sedans executivos até supercarros esportivos, a empresa se destaca por atender às mais altas expectativas de qualidade e exclusividade.
-                            Cada carro é cuidadosamente selecionado para proporcionar uma experiência premium, seja para uma viagem de negócios, uma ocasião especial ou simplesmente para aproveitar a adrenalina de dirigir um veículo de alto desempenho.
-                            Nosso compromisso é unir luxo e praticidade, permitindo que nossos clientes desfrutem de uma experiência memorável a cada reserva.
+                        <!--titleSS - titleSecondSection-->
+                        <h2 class="tituloGeral title" id="titleSS">ABOUT US</h2>
+                        <p><strong>Fast & Furious Cars Inc.</strong> is a leading company in the luxury car rental market. We offer a unique experience for clients seeking elegance, comfort, and quality.
+                            Our fleet consists of the most sophisticated vehicles on the market, from executive sedans to sports cars, and we stand out for our exclusivity.
+                        </p>
+                        <br>
+                        <p>Each car is carefully selected to provide an unforgettable experience, whether for a business trip, a special occasion, or simply enjoying the thrill of driving.
+                            Our greatest commitment is to unite luxury and practicality, allowing our clients to enjoy a memorable experience with every booking.
                         </p>
                         <br>
                         <p>
-                            Além da excelência na frota, a <strong>Fast & Furious Cars Inc.</strong> investe em tecnologia de ponta para oferecer um sistema de reservas online simples e eficiente.
-                            A plataforma permite que os clientes pesquisem e escolham o carro ideal com base em critérios como marca, estilo e preço, agendando o período desejado em poucos cliques.
-                            Para os administradores, o sistema fornece ferramentas avançadas de gestão, como controle de visibilidade de veículos e estatísticas em tempo real, otimizando a operação do negócio.
-                            Nosso objetivo é redefinir o setor de aluguel de carros de luxo, combinando uma frota exclusiva com serviços personalizados que priorizam a satisfação do cliente.
+                            In addition to our exceptional fleet, we invest in cutting-edge technology to offer a simple and efficient online booking system.
+                            The platform allows clients to search for and select the perfect car based on criteria such as brand, style, and price, scheduling the desired rental period in just a few clicks.
+                            For administrators, the system provides advanced management tools, such as vehicle visibility controls and real-time statistics, optimizing business operations.
+                            Our goal is to redefine the luxury car rental sector by combining exclusivity with personalized services that prioritize customer satisfaction.
                         </p>
                     </div>
                 </div>
             </section>
             <section class="thirdSection" id="thirdSection">
                 <div class="checkthisout">
-                    <div class="containerPainel">
-                        <h2 class="tituloGeral title">STATS</h2>
+                    <!--titleTS - titleThirdSection-->
+                    <h2 class="tituloGeral title" id="titleTS">CHECK THIS OUT</h2>
 
-                    </div>
                 </div>
             </section>
-        </article>
     </main>
     <footer class="textoGeral fourthSection" id="fourthSection">
         <div class="conteudoFooter" id="aluno">
@@ -92,7 +94,7 @@
             </div>
         </div>
     </footer>
-    <script src="/JS/script.js"></script>
+    <script src="/JS/homepage.js"></script>
 </body>
 
 </html>
